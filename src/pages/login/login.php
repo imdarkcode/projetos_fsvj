6af61b1b0f74302c9f4d182c7729f7b2e6a2799d
imdarkcode <otavio.sudano13@gmail.com>
2025-05-28 13:48:23 -0300

    feat: pagina de login

--- src/pages/login/login.php
+++ src/pages/login/login.php
@@ -1,31 +1,49 @@
-<body>
-  <section class="imagem-container">
-    <img src="../../assets/images/imagem_login.jpg" alt="Imagem de login" />
-  </section>
+<!DOCTYPE html>
 
-  <section class="login-container">
-    <h1>ProjetosFSVJ</h1>
-    <p class="descricao">Faça login para acessar ou gerenciar seus projetos de forma prática e organizada</p>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Projetos FSVJ - Login</title>
+  </head>
 
-    <form>
-      <label for="email">E-mail</label>
-      <input type="text" id="email" placeholder="Digite seu e-mail" />
+  <body>
+    <section class="imagem-container">
+      <img src="../../assets/images/imagem_login.jpg" alt="Imagem de login" />
+    </section>
 
-      <label for="senha">Senha</label>
-      <div class="senha-container">
-        <input type="password" id="senha" placeholder="Digite sua senha" />
-        <i class="bi bi-eye-slash-fill eye"></i>
-      </div>
+    <section class="login-container">
+      <h1>ProjetosFSVJ</h1>
+      <p class="descricao">Faça login para acessar ou gerenciar seus projetos de forma prática e organizada</p>
 
-      <a href="#" class="esqueci-senha">Esqueceu sua senha?</a>
-      <p class="erro">E-mail ou senha inválidos</p>
+      <form method="POST">
+        <label for="email">E-mail</label>
+        <input type="text" id="email" name="email" placeholder="Digite seu e-mail" />
 
-      <button type="submit">Entrar</button>
-    </form>
+        <label for="senha">Senha</label>
+        <div class="senha-container">
+          <input type="password" id="senha" name="senha" placeholder="Digite sua senha" />
+          <i class="bi bi-eye-slash-fill eye"></i>
+        </div>
 
-    <p class="rodape">
-      Este ambiente é exclusivo para usuários autorizados do <strong>Fundo Social Vale do Jequitinhonha</strong>
-    </p>
-  </section>
-</body>
+        <a href="#" class="esqueci-senha">Esqueceu sua senha?</a>
+        <p class="erro">E-mail ou senha inválidos</p>
+
+        <button type="submit">Entrar</button>
+      </form>
+
+      <p class="rodape">
+        Este ambiente é exclusivo para usuários autorizados do <strong>Fundo Social Vale do Jequitinhonha</strong>
+      </p>
+    </section>
+  </body>
+</html>
+
+<?php
+  $email = $_POST["email"];
+  $senha = $_POST["senha"];
+
+  echo $_POST["email"];
+?>
 
