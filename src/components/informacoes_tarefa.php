15e91ab741fac6ed9daec729d72be46c0ed7aae7
imdarkcode <otavio.sudano13@gmail.com>
2025-06-04 17:39:05 -0300

    feat: pagina de fase e estilo global

--- src/components/informacoes_tarefa.php
+++ src/components/informacoes_tarefa.php
@@ -6,16 +6,19 @@
     $id_coordenador = $row_projeto_fase["ID_COORDENADOR"];
 
     if ($hierarquia == "VOLUNTÁRIO" or $hierarquia == "COORDENADOR" and $id_coordenador != $id_usuario) {
-        $sql = "SELECT * FROM TAREFAS T INNER JOIN USUARIOS_TAREFAS UT ON UT.ID_TAREFA = T.ID_TAREFA WHERE T.ESTADO = 'A FAZER' AND UT.ID_USUARIO = '$id_usuario'";
+        $sql = "SELECT T.ID_TAREFA, T.NOME, T.DATA_VENCIMENTO FROM TAREFAS T INNER JOIN USUARIOS_TAREFAS UT ON UT.ID_TAREFA = T.ID_TAREFA WHERE T.ESTADO = 'A FAZER' AND UT.ID_USUARIO = '$id_usuario'";
         $query_tarefa = $mysqli -> query($sql);
     }
     else {
-        $sql = "SELECT * FROM TAREFAS T INNER JOIN USUARIOS_TAREFAS UT ON UT.ID_TAREFA = T.ID_TAREFA WHERE T.ESTADO = 'A FAZER'";
+        $sql = "SELECT T.ID_TAREFA, T.NOME, T.DATA_VENCIMENTO, T.DESCRICAO FROM TAREFAS T INNER JOIN USUARIOS_TAREFAS UT ON UT.ID_TAREFA = T.ID_TAREFA WHERE T.ESTADO = 'A FAZER'";
         $query_tarefa = $mysqli -> query($sql);
     }
 
     while ($row_tarefa = $query_tarefa -> fetch_assoc()) {
         $id_tarefa = $row_tarefa["ID_TAREFA"];
+        $nome_tarefa = $row_tarefa["NOME"];
+        $data_vencimento = $row_tarefa["DATA_VENCIMENTO"];
+        $descricao_tarefa = $row_tarefa["DESCRICAO"];
         
         echo '
             <section id="informacoesTarefa'.$id_tarefa.'" class="informacoes-tarefa">
@@ -27,7 +30,17 @@
                         </div>
 
                         <div class="card-main">
-                            
+                            <div class="card-informacoes-tarefa-header">
+                                <div>
+                                    <h3>'.$nome_tarefa.'</h3>
+                                    <p>Vence dia '.$data_vencimento.'</p>
+                                </div>
+                            </div>
+
+                            <div class="input-group input-escopo">
+                                <label>Descrição</label>
+                                <textarea disabled>'.$descricao_tarefa.'</textarea>
+                            </div>
                         </div>
                     </div>
                 </div>
